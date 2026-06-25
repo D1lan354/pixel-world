340d1d53ca4d2415be049c9005d02a750fae402e
D1lan354 <rodimushkind@gmail.com>
2026-06-26 02:18:25 +0300

    Update database.js

--- database.js
+++ database.js
@@ -7,3 +7,24 @@ const firebaseConfig = {
   messagingSenderId: "987006631539",
   appId: "1:987006631539:web:8f8b732de1d26fa2ce3406"
 };
+
+// Запускаємо зв'язок з серверами
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+const mapRef = db.ref('multiplayer_map');
+
+let mapData = {};
+
+// Слухаємо онлайн базу: коли міняється хоч один піксель — перемальовуємо мапу
+mapRef.on('value', (snapshot) => {
+    mapData = snapshot.val() || {};
+    if (typeof redrawCanvas === "function") {
+        redrawCanvas();
+    }
+});
+
+// Функція відправки пікселя на сервер Google
+function sendPixel(x, y, colorCode) {
+    let key = x + '_' + y;
+    db.ref('multiplayer_map/' + key).set(colorCode);
+}
