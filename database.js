c1565567feac38ea019c45ded4663caa837bc574
D1lan354 <rodimushkind@gmail.com>
2026-06-26 02:48:05 +0300

    Update database.js

--- database.js
+++ database.js
@@ -1,3 +1,4 @@
+// Конфігурація твого проєкту Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyArIgEB93kQ_2xfJAjkvGuKVhYqwoKpB54",
   authDomain: "pixel-world-db.firebaseapp.com",
@@ -8,24 +9,26 @@ const firebaseConfig = {
   appId: "1:987006631539:web:8f8b732de1d26fa2ce3406"
 };
 
+// Запуск зв'язку з Firebase
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
 }
 const db = firebase.database();
 const mapRef = db.ref('multiplayer_map');
 
-// Робимо масив глобальним
+// Створюємо глобальну змінну, яку бачитиме файл player.js
 window.mapData = {};
 
-// Слухаємо базу в реальному часі
+// Головний слухач онлайну: коли хтось ставить піксель, Firebase миттєво оновлює карту у всіх
 mapRef.on('value', (snapshot) => {
     window.mapData = snapshot.val() || {};
-    // Викликаємо перемальовування екрану
+    // Примусово викликаємо функцію малювання з файлу player.js
     if (typeof window.redrawCanvas === "function") {
         window.redrawCanvas();
     }
 });
 
+// Функція, яка закидає твій клік в інтернет
 function sendPixel(x, y, colorCode) {
     let key = x + '_' + y;
     db.ref('multiplayer_map/' + key).set(colorCode);
