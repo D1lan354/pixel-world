1ed36876d39cc51f909ae1d3afda40fa97f12e5f
D1lan354 <rodimushkind@gmail.com>
2026-06-26 15:39:48 +0300

    Update player.js

--- player.js
+++ player.js
@@ -14,6 +14,18 @@ function sendPixel(x, y, colorCode) {
     
     return true; 
 }
+function loadMapFromFirebase() {
+    console.log("Завантаження карти з гілки multiplayer_map...");
+    // Вказуємо конкретний шлях, щоб не тягнути зайве
+    db.ref('multiplayer_map').on('value', (snapshot) => {
+        const data = snapshot.val();
+        if (data) {
+            window.mapData = data; // Записуємо у глобальну змінну
+            renderFullMap();        // Викликаємо функцію малювання
+            console.log("Карта завантажена!");
+        }
+    });
+}
 
 // ==========================================
 // 2. ВИПРАВЛЕНЕ ЗЧИТУВАННЯ (для player.js)
