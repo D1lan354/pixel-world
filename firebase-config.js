1d74d661dd0e8bc33d8344e3ca4a46ca9d92c868
D1lan354 <rodimushkind@gmail.com>
2026-06-27 23:59:03 +0300

    Update Firebase config with placeholder values
    Replaced Firebase configuration values with placeholders for user input.

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-
+ 
+// 👇 ЗАМІНИ НА СВОЇ ЗНАЧЕННЯ
 const firebaseConfig = {
-  apiKey: "AIzaSyDCKmkm7PeB2s2xJ0RLJKRmOLK9kThrAvY",
-  authDomain: "pixeledworld.firebaseapp.com",
-  projectId: "pixeledworld",
-  storageBucket: "pixeledworld.firebasestorage.app",
-  messagingSenderId: "862353757400",
-  appId: "1:862353757400:web:2cfd449f93a9fc3eac73ad",
-  measurementId: "G-R05NR5YMQ6"
+  apiKey:            "ТВІЙ_API_KEY",
+  authDomain:        "ТВІЙ_PROJECT.firebaseapp.com",
+  projectId:         "ТВІЙ_PROJECT_ID",
+  storageBucket:     "ТВІЙ_PROJECT.appspot.com",
+  messagingSenderId: "ТВІЙ_SENDER_ID",
+  appId:             "ТВІЙ_APP_ID"
 };
-
+ 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+export const db   = getFirestore(app);
 export const auth = getAuth(app);
+ 
