f983d21ef25bfe7614cae68982608211a67702a3
D1lan354 <rodimushkind@gmail.com>
2026-06-28 09:53:32 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
- 
-// 👇 ЗАМІНИ НА СВОЇ ЗНАЧЕННЯ
 const firebaseConfig = {
   apiKey: "AIzaSyDCKmkm7PeB2s2xJ0RLJKRmOLK9kThrAvY",
   authDomain: "pixeledworld.firebaseapp.com",
@@ -13,8 +8,5 @@ const firebaseConfig = {
   appId: "1:862353757400:web:2cfd449f93a9fc3eac73ad",
   measurementId: "G-R05NR5YMQ6"
 };
- 
-const app = initializeApp(firebaseConfig);
-export const db   = getFirestore(app);
-export const auth = getAuth(app);
- 
+
+window.FIREBASE_CONFIG = firebaseConfig;
